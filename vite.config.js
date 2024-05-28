b4debaea093dbbf37ecd3d5a8950f0cd40bf199c
Marmik-001 <marmikrshah@gmail.com>
2024-05-28 17:54:11 +0530

    change in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,21 @@ import react from '@vitejs/plugin-react'
 // import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactIcons from 'vite-plugin-react-icons';
 // https://vitejs.dev/config/ 
+
 export default defineConfig({
   plugins: [
     react(),
     // reactRefresh(),
     // reactIcons(),
   ],
-})
+});
+
+
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
